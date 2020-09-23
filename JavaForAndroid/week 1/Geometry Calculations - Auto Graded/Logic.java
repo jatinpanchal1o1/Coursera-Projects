d70c21950c664269328ef54e67682bb5a3d6f086
jatinpanchal1o1 <50742576+jatinpanchal1o1@users.noreply.github.com>
2020-09-23 21:43:40 +0530

    2020

---
+++ JavaForAndroid/week 1/Geometry Calculations - Auto Graded/Logic.java
@@ -0,0 +1,43 @@
+class Logic{
+    public static double rectangleArea(double length, double width){
+        
+        
+    }
+public static double rectanglePerimeter(double length, double width){
+    
+    
+}
+public static double circleArea(double radius){
+    
+    
+}
+public static double circleCircumference(double radius){
+    
+    
+}
+public static double rightTriangleArea(double base, double height){
+    
+    
+}
+public static double rightTrianglePerimeter(double base, double height){
+    
+    
+}
+public static double boxVolume(double length, double width, double depth){
+    
+    
+}
+public static double boxSurfaceArea(double length, double width,
+ double depth){
+     
+     
+ }
+public static double sphereVolume(double radius){
+    
+    
+}
+public static double sphereSurfaceArea(double radius){
+    
+    
+}
+}
